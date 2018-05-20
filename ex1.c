d502dce48830103d3899f3fbcd6ab5d2b395d1bd
Paul Runnoe <runnoe.paul@gmail.com>
2018-05-19 23:03:05 -0500

    finished ex1

--- ex1.c
+++ ex1.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
-
-/* This is a comment. */
+# include <stdio.h>
 int main(int argc, char *argv[])
 {
     int distance = 100;
 
-    // this is also a comment
     printf("You are %d miles away.\n", distance);
 
     return 0;
